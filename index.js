369b7f77ff227be47e81feea6162252cc1e57e36
Mattie <Mbt.jghs@gmail.com>
2025-09-18 13:44:37 +0200

    combined

--- index.js
+++ index.js
@@ -13,3 +13,36 @@ function save() {
     countEl.textContent = 0
     count = 0
 }
+
+let homeScoreBtnOne = document.getElementById("home-score-btn-1")
+let homeStoreEl =document.getElementById("home-score")
+let homeScore = 0
+
+function increaseHomeScoreOne(){
+    homeScore += 1
+    homeStoreEl.textContent = homeScore
+}
+function increaseHomeScoreTwo(){
+    homeScore += 0.5
+    homeStoreEl.textContent = homeScore
+}
+
+let awayScoreBtnOne = document.getElementById("away-score-btn-1")
+let awayStoreEl =document.getElementById("away-score")
+let awayScore = 0
+
+function increaseAwayScoreOne(){
+    awayScore += 1
+    awayStoreEl.textContent = awayScore
+}
+function increaseAwayScoreTwo(){
+    awayScore += 0.5
+    awayStoreEl.textContent = awayScore
+}
+
+function resetScore(){
+    awayScore = 0
+    awayStoreEl.textContent = awayScore
+    homeScore = 0
+    homeStoreEl.textContent = homeScore
+}
